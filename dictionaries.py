285371daabd71132ccac95266c57b7e5bbbc3935
Kelsey and Laura <laura.m.matchett@gmail.com>
2015-12-02 21:25:51 -0800

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+#my_stats = {"name": "laura", "age": 10, "height": 100}
+
+#del my_stats["age"]
+#print my_stats
+#print "%s is %i years old with a height of %i." % (my_stats["name"], my_stats["age"], my_stats["height"])
+
+#vocab_words = {"boolean": True, "loop": "cycle", "print": "show the result", "conditional": "while"}
+
